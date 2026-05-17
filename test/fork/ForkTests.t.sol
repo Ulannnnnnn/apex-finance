8737646f505669651ac4e13b074995426fcaf2d6
unknown <nursaamirman@gmail.com>
2026-05-17 18:08:52 +0500

    test: implement mainnet fork tests for Chainlink and real USDC interaction

--- test/fork/ForkTests.t.sol
+++ test/fork/ForkTests.t.sol
@@ -33,7 +33,7 @@ contract ForkTest is Test {
     /// @dev Uniswap V2 Router
     address constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
-    /// @dev WETH на mainnet
+    /// @dev WETH on mainnet
     address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     uint256 forkId;
